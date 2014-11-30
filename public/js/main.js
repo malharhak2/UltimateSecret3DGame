a427a63a5f6c3a3f09459342f65d6f13ff6a5782
Anthony Pigeot <anthony.pigeot@gmail.com>
2014-11-30 02:05:31 +0100

    Three.js test initialization

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,37 @@
 requirejs.config ({
-	baseUrl: 'js/'
+	baseUrl: 'js/',
+	paths: {
+		"domready" : "../libs/requirejs-domready/domReady"
+	},
+	urlArgs : "d=" + Date.now()
 });
 
-require([], function () {
+require(["domready"], function () {
 	console.log('Hello world');
+
+	// Just some test code
+	console.log(THREE);
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	// Cube
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	var cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	camera.position.z = 5;
+
+	// Render
+	function render() {
+		cube.rotation.x += 0.1;
+		cube.rotation.y += 0.1;
+		requestAnimationFrame( render );
+		renderer.render( scene, camera );
+	}
+	render();
 });
